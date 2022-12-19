7444c5770516093984a37c6d4a3450d1d1119e00
Romario Curcio <romario.curcio@shapedigital.com>
2022-12-19 14:01:35 -0300

    chore: build startup

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '.', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, '.', 'build'));
+  res.sendFile(path.join(__dirname, '..', 'build'));
 });
 app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, '.', 'build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
 });
 app.listen(port, ()=>{
   console.log("Server is running on port: ", port)
